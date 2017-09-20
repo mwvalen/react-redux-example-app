3e63b39e18ccddd34c55121109f5d4ac8d4ab08a
Michael W. Valentine <mwillvalentine@gmail.com>
2017-09-20 16:46:56 -0400

    first commit - added store, root reducer, default state to boilerplate app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import {createStore} from 'redux';
+
+const defaultState = { checked: false };
+const reducer = function(state = defaultState, action) {
+  switch(action.type) {
+    case 'TOGGLE':
+      return { ...state, checked: !state.checked };
+  }
+  return state;
+};
+const store = createStore(reducer);
 
 class App extends React.Component {
   render() {

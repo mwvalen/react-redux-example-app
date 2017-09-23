0cdfe768a1e8dcb5a77749941b7dec171d64197b
Michael W. Valentine <mwillvalentine@gmail.com>
2017-09-23 17:40:11 -0400

    Added react router, with browserHistory. Added route components, and login page.

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import React from 'react';
 import store from './store';
 import Home from './components/Home';
 import Login from './components/Login';
-import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 import App from './components/App';
 
 ReactDOM.render((
   <Provider store={store}>
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App}>
     <IndexRoute component={Home} />
     <Route path="login" component={Login} />

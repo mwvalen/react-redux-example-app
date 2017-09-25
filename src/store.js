e316a50cfdd0d8a408728ed5efdc9b67a8d9e1cd
mwvalen <mwillvalentine@gmail.com>
2017-09-25 12:37:08 -0400

    Refactored reducer into multiple reducers.

--- src/store.js
+++ src/store.js
@@ -1,19 +1,19 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore, combineReducers } from 'redux';
 import { promiseMiddleware } from './middleware';
 
-const defaultState = {
-  appName: 'conduit',
-  articles: null
-};
-const reducer = function(state = defaultState, action) {
-  switch(action.type) {
-    case 'HOME_PAGE_LOADED':
-      return { ...state, articles: action.payload.articles };
-    }
+import auth from './reducers/auth';
+import common from './reducers/common';
+import home from './reducers/home';
 
-  return state;
-};
 
-const store = createStore(reducer, applyMiddleware(promiseMiddleware));
+const reducer = combineReducers({
+  auth,
+  common,
+  home
+});
+
+const middleware = applyMiddleware(promiseMiddleware);
+
+const store = createStore(reducer, middleware);
 
 export default store;
